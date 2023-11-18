9626109abb2aaa32bcdd8cc717486ebacbf0dcaa
Patric Green <bagro@bagro.se>
2023-11-18 21:07:37 +0100

    Fix label colors

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -36,14 +36,14 @@ function onSubmit(values, {setErrors}) {
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <Form @submit="onSubmit" :validation-schema="schema" v-slot="{ errors, isSubmitting }" class="space-y-6">
         <div>
-          <label for="email" class="block text-sm font-medium leading-6 ">Email address</label>
+          <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
           <div class="mt-2">
             <Field name="email" type="email" :class="errors.email ? 'input-error' : ''"
                    class="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
           </div>
         </div>
         <div>
-          <label for="password" class="block text-sm font-medium leading-6 ">Password</label>
+          <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
           <div class="mt-2">
             <Field name="password" type="password" :class="errors.password ? 'input-error' : ''"
                    class="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
